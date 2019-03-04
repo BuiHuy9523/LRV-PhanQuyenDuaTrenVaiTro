4d7ab95b39825f777f14385d9092d0206bf80e59
HuyPC <sinhhuy.bui@gmail.com>
2019-03-04 16:22:16 +0700

    'phanquyen'

---
+++ resources/views/page_admin.blade.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HuyPC
+ * Date: 3/4/2019
+ * Time: 2:18 PM
+ */
+
+echo"xin chao admin";
+?>
---
+++ resources/views/page_guest.blade.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HuyPC
+ * Date: 3/4/2019
+ * Time: 2:17 PM
+ */
+ echo 'xin chao guest';
